150700b132bd1645f0fa3c3180ebaea6ae13121f
Draraedus <adriel2002santos@gmail.com>
2025-01-19 16:02:56 -0300

    adicionada funcões de adicionar, atualizar, sortear e reicinar sorteio

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+let amigoSorteado = '';
+
+function adicionarAmigo() {
+  let nomeAmigo = document.getElementById("amigo");
+  if(amigoSorteado !== '') {
+    reiniciarSorteio();
+  }
+  if (nomeAmigo.value === "") {
+    alert("Por favor, insira um nome.");
+    return;
+  }
+  amigos.push(nomeAmigo.value);
+  nomeAmigo.value = "";
+  atualizarListarAmigos();
+}
+
+function atualizarListarAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  amigos.forEach((amigo) => {
+    let AmigoParaLista = document.createElement("li");
+    AmigoParaLista.innerHTML = amigo;
+    listaAmigos.appendChild(AmigoParaLista);
+  });
+}
+
+function sortearAmigo() {
+  let tamanhoListaAmigos = amigos.length;
+  let listarAmigoSorteado = document.getElementById("resultado");
+  if (tamanhoListaAmigos === 0) {
+    alert("Por favor, adicione amigos para sortear.");
+    return;
+  }
+    let numeroAmigoSorteado = Math.floor(Math.random() * tamanhoListaAmigos);
+    amigoSorteado = amigos[numeroAmigoSorteado];
+    listarAmigoSorteado.innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+    amigos = [];
+    atualizarListarAmigos();
+}
+
+
+function reiniciarSorteio() {
+  let listarAmigoSorteado = document.getElementById("resultado");
+  listarAmigoSorteado.innerHTML = "";
+  amigoSorteado = '';
+}
